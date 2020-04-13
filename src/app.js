0c33b81c9b08d3638d8464a2e98ac55a6bd55160
bhavinsa <bhavinpatelsa@gmail.com>
2020-04-13 15:25:41 +0530

    testing api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const port = process.env.PORT
-const userRouter = require('./routers/user')
-const projectRouter = require('./routers/project')
-require('./db/db')
+const express = require('express');
+const port = process.env.PORT;
+const userRouter = require('./routers/user');
+const projectRouter = require('./routers/project');
+require('./db/db');
 
-const app = express()
+const app = express();
 
-app.use(express.json({ limit: '1mb' }))
+app.use(express.json({ limit: '1mb' }));
 // app.use(express.urlencoded({ extended: false }));
-app.use('/user', userRouter)
-app.use('/project', projectRouter)
+app.use('/user', userRouter);
+app.use('/project', projectRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
---
+++ test/auth.js
@@ -0,0 +1,79 @@
+const { chai, server, should } = require('./testConfig');
+const UserModel = require('../src/models/User');
+
+/**
+ * Test cases to test all the authentication APIs
+ * Covered Routes:
+ * (1) Register Error
+ * (2) Register
+ * (3) Get Data
+ */
+
+describe('Auth', () => {
+  // Before each test we empty the database
+  before((done) => {
+    UserModel.deleteMany({}, (err) => {
+      done();
+    });
+  });
+
+  // Prepare data for testing
+  const testData = {
+    name: 'test',
+    password: 'Test@123',
+    email: 'bhavin@test12345.com',
+  };
+
+  /*
+   * Test the /POST route
+   */
+  describe('/POST Register', () => {
+    it('It should send validation error for Register', (done) => {
+      chai
+        .request(server)
+        .post('/user')
+        .send({ email: testData.email })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /POST route
+   */
+  describe('/POST Register', () => {
+    it('It should Register user', (done) => {
+      chai
+        .request(server)
+        .post('/user')
+        .send(testData)
+        .end((err, res) => {
+          console.log(res.body);
+          res.should.have.status(201);
+          res.body.should.have.property('name').eql(testData.name);
+          testData.token = res.body.token;
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /POST route
+   */
+  describe('/POST Register', () => {
+    it('It should return user data', (done) => {
+      chai
+        .request(server)
+        .post('/user/me')
+        .send()
+        .set('Authorization', 'Bearer ' + testData.token)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('name').eql(testData.name);
+          done();
+        });
+    });
+  });
+});
